b0264a0d865182618315b0a7ef8dbd89c6a6d5a5
asmafalah92 <asma.falah92@gmail.com>
2015-09-26 00:56:32 +0300

    2nd course project

--- plot3.R
+++ plot3.R
@@ -1,6 +1,6 @@
 ##3. Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, which of these four sources have seen decreases in emissions from 1999 to 2008 for Baltimore City?
 ##Which have seen increases in emissions from 1999 to 2008? Use the ggplot2 plotting system to make a plot to answer this question.
-##Please see "Downloading and reading the data" file in the repository if you're wondeing about
+##Please see "Downloading and reading the data" file in this repository if you're wondering about
 ## the whereabouts of the code for downloading and reading the data.
 ##Loading the packages
 library(ggplot2)
@@ -16,7 +16,7 @@ colnames(totalpm2.5Balt2)[3] <- "Emissions"
 ## onto plotting
 ## plotting to PNG
 png("plot3.png") 
-qplot(year, Emissions, data = totalpm2.5Balt2, color=type, geom ="line") + ggtitle(expression("Baltimore Maryland" ~ PM[2.5] ~ "Emmission by source, type, and year")) + xlab("Year") + ylab(expression("Total" ~ PM[2.5] ~ "Emissions (tons)"))
+qplot(year, Emissions, data = totalpm2.5Balt2, color=type, geom ="line") + geom_point( size=5, shape=21, fill="white") + ggtitle(expression("Baltimore Maryland" ~ PM[2.5] ~ "Emmission by source, type, and year")) + xlab("Year") + ylab(expression("Total" ~ PM[2.5] ~ "Emissions (tons)"))
 dev.off()
 
 ## Answer of Question 3:
